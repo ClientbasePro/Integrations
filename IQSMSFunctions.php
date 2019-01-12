a3a4b61a5ca496067b9b4edafddf71073a020d7d
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2019-01-12 14:58:05 +0300

    Обновлена ф-я отправки СМС через IQSMS
    добавлены 2 параметра для сохранения СМС в лог:
    4. $needSave (bool) - сохранять или нет
    5. $data - массив с доп.полями для сохранения ($fieldId=>$value)

--- IQSMSFunctions.php
+++ IQSMSFunctions.php
@@ -3,20 +3,29 @@
   // файл с функциями для интеграции Clientbase и сервиса СМС-рассылок IQ SMS (https://iqsms.ru/api/api_rest-php/)
 require_once "common.php"; 
 
-    // отправка СМС с текстом $text на телефон $phone от имени отправителя $sender
-function IQSMS_SendSMS($phone, $text, $sender) {
+    // отправка СМС с текстом $text на телефон $phone от имени отправителя $sender, доп.признак $needSave сохраняет СМС в лог, доп.массив $data для сохранения в лог
+function IQSMS_SendSMS($phone, $text, $sender, $needSave=1, $data='') {
   $phone = SetNumber($phone);
   $text = form_input($text);
   if (!$sender) $sender = IQSMS_DEFAULT_NAME;
   if (!$phone || !$text) return 'no input data';
-  $phone = rawurlencode($phone);
-  $text = rawurlencode($text);
+  $phone_ = rawurlencode($phone);
+  $text_ = rawurlencode($text);
   $sender = rawurlencode($sender);
-  $url = IQSMS_URL.'send?phone='.$phone.'&text='.$text.'&sender='.$sender.'&login='.IQSMS_API_LOGIN.'&password='.IQSMS_API_PASSWORD;
+  $url = IQSMS_URL.'send?phone='.$phone_.'&text='.$text_.'&sender='.$sender.'&login='.IQSMS_API_LOGIN.'&password='.IQSMS_API_PASSWORD;
   $curl = curl_init($url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   $tmp = curl_exec($curl);    
   curl_close($curl);
+    // сохранить в лог?
+  if ($needSave && SMSLOG_TABLE) {
+	$sms = explode(";", $tmp);
+	$ins['f'.SMSLOG_FIELD_UID] = intval($sms[1]);
+	$ins['f'.SMSLOG_FIELD_PHONE] = $phone;
+    $ins['f'.SMSLOG_FIELD_TEXT] = $text;
+	if ($data) foreach ($data as $fieldId=>$value) $ins[$fieldId] = $value;
+    data_insert(SMSLOG_TABLE, EVENTS_ENABLE, $ins);  
+  }  
   return $tmp;
 }
 
